fb7276381697f7c4bd8b922cbbe66d76a5f3f700
Nathan Creger <nathancreger1111@gmail.com>
2024-05-03 10:55:20 -0400

    change 30 to 31 keys

--- src/test/java/org/kpmp/packages/PackageKeysTest.java
+++ src/test/java/org/kpmp/packages/PackageKeysTest.java
@@ -8,7 +8,7 @@ public class PackageKeysTest {
 
 	@Test
 	public void testLength() throws Exception {
-		assertEquals(30, PackageKeys.values().length);
+		assertEquals(31, PackageKeys.values().length);
 	}
 
 	@Test
